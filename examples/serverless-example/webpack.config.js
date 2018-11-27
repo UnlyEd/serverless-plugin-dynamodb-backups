c3b9c748a78c82742442a9afb2bc7410d281dbdd
anthony <anthony.troupenat@studylink.fr>
2018-11-27 17:09:39 +0100

    lightens serverless-example configuration

--- examples/serverless-example/webpack.config.js
+++ examples/serverless-example/webpack.config.js
@@ -5,15 +5,6 @@ module.exports = {
   entry: slsw.lib.entries,
   target: 'node',
   mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
-  optimization: {
-    // We no not want to minimize our code.
-    minimize: false,
-  },
-  performance: {
-    // Turn off size warnings for entry points
-    hints: false,
-  },
-  devtool: 'nosources-source-map',
   externals: [nodeExternals()],
   module: {
     rules: [
